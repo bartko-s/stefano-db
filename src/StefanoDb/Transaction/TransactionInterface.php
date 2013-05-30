be13490a412a54d9419a48b15fda7f3645d216d8
Bartko Stefan <bartko.stefan@gmail.com>
2013-05-30 22:11:26 +0200

    Added new TransactionInterface

---
+++ src/StefanoDb/Transaction/TransactionInterface.php
@@ -0,0 +1,29 @@
+<?php
+namespace StefanoDb\Transaction;
+
+interface TransactionInterface
+{
+    /**
+     * Begin transaction if nested transaction has not been opened yet
+     * @return self
+     */
+    public function begin();
+    
+    /**
+     * Commit transaction if nested transaction has not been opened yet
+     * @return self
+     */
+    public function commit();
+    
+    /**
+     * Roolback transaction
+     * @return self
+     */
+    public function roolBack();
+    
+    /**
+     * Return true if transaction is opened
+     * @return boolean
+     */
+    public function isInTransaction();
+}
